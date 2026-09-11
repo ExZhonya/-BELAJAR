937f33f36795e8d65dd7ffcd5a42e3e2825c7f9a
ExZhonya <exzhonya@gmail.com>
2026-09-11 13:31:53 +0700

    added npc

--- TEXT RPG GAME/map_.py
+++ TEXT RPG GAME/map_.py
@@ -1,4 +1,3 @@
-# map_.py
 import random
 from preset import utils as u
 from plr.player import player
@@ -8,24 +7,29 @@ class Base:
 	def camp():
 		u.clear()
 		print(rf"""
-	You are in the Camp.
-	Fuel: {player.fuel}  Food: {player.food}  Health: {player.health} Money: {player.money}
-	1. Find Fuel
-	2. Find Food
-	3. Explore
-	0. Quit(No Save!)
-""")
+		You are in the Camp.
+		Fuel: {player.fuel}  Food: {player.food}  Health: {player.health} Money: {player.money}
+		1. Find Fuel
+		2. Find Food
+		3. Explore
+		9. NPC
+		0. Quit(No Save!)
+		""")
 		x = u.getch()
 		if x == "1":
 			Base.chop_wood()
 		elif x == "2":
 			Base.find_food()
 		elif x == "3":
-			pass
+			Base.camp()
+		elif x == "9":
+			Base.NPC()
 		elif x == "0":
 			from preset import wc_gb
 			wc_gb.bye_asc()
 			wc_gb.bye_text()
+		else:
+			Base.camp()
 
 	@staticmethod
 	def chop_wood():
@@ -43,6 +47,30 @@ class Base:
 		u.getch()
 		Base.camp()
 
+	@staticmethod
+	def NPC():
+		u.clear()
+
+		print("===== NPC =====")
+		if player.trader:
+			print("1. Trader")
+		elif player.hunter:
+			print("2. Hunter")
+		elif player.lumberjack:
+			print("3. Lumberjack")
+		print("0. Back")
+
+		x = u.getch()
+		if x == "1" and player.trader:
+			pass
+		elif x == "2" and player.hunter:
+			pass
+		elif x == "3" and player.lumberjack:
+			pass
+		elif x == "0":
+			Base.camp()
+		else:
+			Base.NPC()
 
 if __name__ == '__main__':
 	Base.camp()
--- TEXT RPG GAME/plr/player.py
+++ TEXT RPG GAME/plr/player.py
@@ -1,9 +1,12 @@
-# player.py
 class Player:
 	def __init__(self):
 		self.fuel = 0
 		self.food = 0
 		self.health = 100
 		self.money = 30
+		self.trader = False
+		self.hunter = False
+		self.lumberjack = False
+		self.base = True
 
 player = Player()
